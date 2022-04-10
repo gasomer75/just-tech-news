9980c09dc3a67f1c364c14cca12370ca56aaf64a
gsomer <gasomer75@hotmail.com>
2022-04-10 11:40:51 -0700

    add login and logout scripts

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
+require('dotenv').config();
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +26,7 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
